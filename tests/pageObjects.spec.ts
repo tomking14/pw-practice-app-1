f2e8788fec7d7abe38c18061c4ce338077cedf80
tomking14 <tomykastor@gmail.com>
2026-05-14 14:58:04 -0400

    testing

--- tests/pageObjects.spec.ts
+++ tests/pageObjects.spec.ts
@@ -7,37 +7,37 @@ test.beforeEach(async({page}) =>{
 
 })
 
-test('navigate to form page', async ({page})=>{
-    const pm = new PageManager(page)
-    await pm.navigateTo().formLayoutsPage()
-    await pm.navigateTo().datePickerPage()
-    await pm.navigateTo().smartTabPage()
-    await pm.navigateTo().toastrPage()
-    await pm.navigateTo().tooltipPage()
-})
-test('parameterized methods', async ({page})=>{
-    const pm = new PageManager(page)
-    const randomFullName = faker.person.fullName()
-    // replace is a js method to change one value with another of a string
-    const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
-// page manger will call the method to return the page object that was created in the page manager ts.
-// imagine the same way you construct the object instance here, instead it was done in the pm manager
-    await pm.navigateTo().formLayoutsPage()
-    await pm.onFormsLayoutPage().submitUsingGridFormWithCredentialsAndSelectOption('test@test.com','moretesting123','Option 1')
+// test('navigate to form page', async ({page})=>{
+//     const pm = new PageManager(page)
+//     await pm.navigateTo().formLayoutsPage()
+//     await pm.navigateTo().datePickerPage()
+//     await pm.navigateTo().smartTabPage()
+//     await pm.navigateTo().toastrPage()
+//     await pm.navigateTo().tooltipPage()
+// })
+// test('parameterized methods', async ({page})=>{
+//     const pm = new PageManager(page)
+//     const randomFullName = faker.person.fullName()
+//     // replace is a js method to change one value with another of a string
+//     const randomEmail = `${randomFullName.replace(' ', '')}${faker.number.int(1000)}@test.com`
+// // page manger will call the method to return the page object that was created in the page manager ts.
+// // imagine the same way you construct the object instance here, instead it was done in the pm manager
+//     await pm.navigateTo().formLayoutsPage()
+//     await pm.onFormsLayoutPage().submitUsingGridFormWithCredentialsAndSelectOption('test@test.com','moretesting123','Option 1')
 
-    // screenshotting right after a certain step of a test was completed. give a path and name of the screenshot
-    await page.screenshot({path: 'screenshots/formLayoutsPage.png'})
-    await pm.onFormsLayoutPage().submitInlineFormWithNameAndCheckbox(randomFullName, randomEmail, true)
-    // get a screenshot of only a specific area on the page
-    await page.locator('nb-card', {hasText: 'Inline form'}).screenshot({path: 'screenshots/formLayoutsPage.png'})
-    await pm.navigateTo().datePickerPage()
-    await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(5)
-    await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(3,12)
+//     // screenshotting right after a certain step of a test was completed. give a path and name of the screenshot
+//     await page.screenshot({path: 'screenshots/formLayoutsPage.png'})
+//     await pm.onFormsLayoutPage().submitInlineFormWithNameAndCheckbox(randomFullName, randomEmail, true)
+//     // get a screenshot of only a specific area on the page
+//     await page.locator('nb-card', {hasText: 'Inline form'}).screenshot({path: 'screenshots/formLayoutsPage.png'})
+//     await pm.navigateTo().datePickerPage()
+//     await pm.onDatepickerPage().selectCommonDatepickerDateFromToday(5)
+//     await pm.onDatepickerPage().selectDatepickerWithRangeFromToday(3,12)
 
 
 
-})
-test.only('testing with argos ci', async ({page})=>{
+// })
+test('testing with argos ci', async ({page})=>{
     const pm = new PageManager(page)
     await pm.navigateTo().formLayoutsPage()
     await pm.navigateTo().datePickerPage()
